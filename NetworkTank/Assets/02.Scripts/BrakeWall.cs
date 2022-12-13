fd76b8939029c296fb8c78e78de0cc795e6d39c5
20181181송영훈 <20181181@vision.hoseo.edu>
2022-12-14 04:58:15 +0900

    부수는 오브젝트 생성

---
+++ NetworkTank/Assets/02.Scripts/BrakeWall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrakeWall : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
